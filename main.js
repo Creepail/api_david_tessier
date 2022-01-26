398b2f8e521f126e45076dc77267833ad635634d
David Tessier <david.tessier2@edu.devinci.fr>
2022-01-26 18:39:42 +0100

    :construction: Making fetch

---
+++ main.js
@@ -0,0 +1,12 @@
+fetch('https://ipgeolocation.abstractapi.com/v1/?api_key=4f2f4ee4c522424a92ab42a47ec8dadc')
+.then(response => response.json())
+.then(locationData => { 
+    console.log(locationData)
+
+    fetch('http://api.openweathermap.org/data/2.5/weather?lat=' + locationData.latitude + '&lon=' + locationData.longitude +'&appid=1f841ee1973683bd7f799d206838eb20')
+    .then(response => response.json())
+    .then(WeatherData => {
+        console.log(WeatherData)
+    })
+
+})
