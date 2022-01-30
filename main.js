5d6a0ecb87bc06131e59091cdae77d3766af32af
David Tessier <david.tessier2@edu.devinci.fr>
2022-01-30 16:00:37 +0100

    Importing all changes for vercel deployement

--- main.js
+++ main.js
@@ -6,14 +6,15 @@ let time = document.querySelector('.time')
 console.log(time)
 let audioSource = ''
 let timeToUpdate
+let locationInfo 
 
-function checkWeather(){
+async function checkWeather(){
     fetch('https://ipgeolocation.abstractapi.com/v1/?api_key=4f2f4ee4c522424a92ab42a47ec8dadc')
     .then(response => response.json())
     .then(locationData => { 
-        console.log(locationData)
+        locationInfo = locationData
 
-        fetch('http://api.openweathermap.org/data/2.5/weather?lat=' + locationData.latitude + '&lon=' + locationData.longitude +'&appid=1f841ee1973683bd7f799d206838eb20')
+        fetch('http://api.openweathermap.org/data/2.5/weather?lat=' + locationData.latitude + '&lon=' + locationData.longitude +'&appid=1f841ee1973683bd7f799d206838eb20&units=metric')
         .then(response => response.json())
         .then(WeatherData => {
             console.log(WeatherData)
@@ -21,29 +22,35 @@ function checkWeather(){
             if(WeatherData.wind.speed >= 15){
                 audioSource = 'music/windy.mp3'
                 backgroundImg.style.backgroundImage  = 'url("img/windy.png")'
+                reloadAudio()
             }
             else if(WeatherData.weather[0].main == 'Thunderstorm' || WeatherData.weather[0].main == 'Drizzle' || WeatherData.weather[0].main == 'Rain'){
                 audioSource = 'music/rain.mp3'
                 backgroundImg.style.backgroundImage  = 'url("img/rain.png")'
+                reloadAudio()
             }
             else if(WeatherData.weather[0].main == 'Snow'){
                 audioSource = 'music/snow.mp3'
                 backgroundImg.style.backgroundImage  = 'url("img/snow.png")'
+                reloadAudio()
             }
             else if(WeatherData.weather[0].main == 'Fog'){
                 audioSource = 'music/fog.mp3'
                 backgroundImg.style.backgroundImage  = 'url("img/fog.png")'
+                reloadAudio()
             }
             else{
                 audioSource = 'music/neutral.mp3'
                 backgroundImg.style.backgroundImage  = 'url("img/none.png")'
+                reloadAudio()
             }
 
-            dataContainer.innerHTML += '<p>'+ 'Current Weather : ' + WeatherData.weather[0].description  +'</p>' + '<h1>'+ locationData.city +'</h1>'
+            dataContainer.innerHTML = '<p class="time"></p>' + '<p>'+ 'Current Weather : ' + WeatherData.weather[0].description  +'</p>' + '<p>'+ WeatherData.main.feels_like +' *C</p>'   + '<h1>'+ locationData.city +'</h1>'
 
         })
         timeToUpdate = locationData.timezone.current_time.split(":")
 
+        console.log(audioSource)
 
     })
 }
@@ -67,13 +74,13 @@ function updateTime(){
         
     }
 
-    if(timeToUpdate[1] == 60){
+    if(timeToUpdate[1] > 59){
         timeToUpdate[1] = 0;
         timeToUpdate[0] += 1;
         
     }
 
-    if(timeToUpdate[0] == 24){
+    if(timeToUpdate[0] > 24){
         timeToUpdate[0] = 0;
         
     }
@@ -105,3 +112,75 @@ function startAudio(){
     startMenu.style.display= 'none';
 }
 
+
+function reloadAudio(){
+    audioPlayer.src= audioSource
+    audioPlayer.play()
+}
+
+let screenType = 0;
+
+function seeWorldEvil(){
+
+    if(screenType == 0 ){
+
+        
+    fetch('http://api.openweathermap.org/data/2.5/air_pollution?lat=' + locationInfo.latitude + '&lon=' + locationInfo.longitude +'&appid=1f841ee1973683bd7f799d206838eb20')
+    .then(response => response.json())
+    .then(polutionInfo => {
+        audioSource = 'music/corruption.mp3'
+        reloadAudio()
+        backgroundImg.style.backgroundImage  = 'url("img/corruption.png")'
+        document.querySelector('.worldEvil').innerHTML = '<p>World weather</p>'
+
+
+        if(polutionInfo.list[0].main.aqi == 1 ){
+            dataContainer.innerHTML = '<p class="time"></p>' + '<p>Your world is 20% corrupted (air quality is good)</p>'
+
+        }
+        else if(polutionInfo.list[0].main.aqi == 2 ){
+            dataContainer.innerHTML = '<p class="time"></p>' + '<p>Your world is 40% corrupted (air quality is fair)</p>' 
+
+        }
+
+        else if(polutionInfo.list[0].main.aqi == 3 ){
+            dataContainer.innerHTML = '<p class="time"></p>' + '<p>Your world is 60% corrupted (air quality is moderate)</p>' 
+
+        }
+        else if(polutionInfo.list[0].main.aqi == 4 ){
+            dataContainer.innerHTML = '<p class="time"></p>' + '<p>Your world is 80% corrupted (air quality is poor)</p>' 
+
+        }
+        else if(polutionInfo.list[0].main.aqi == 5 ){
+            dataContainer.innerHTML = '<p class="time"></p>' + '<p>Your world is 100% corrupted (air quality is very poor)</p>' 
+
+        }
+
+        dataContainer.innerHTML += '<p> Air components : <br> co: ' 
+        + polutionInfo.list[0].components.co + '<br> nh3: '
+        + polutionInfo.list[0].components.nh3 + '<br> no: '  
+        + polutionInfo.list[0].components.no + '<br> no2: '  
+        + polutionInfo.list[0].components.no2 + '<br> o3: '  
+        + polutionInfo.list[0].components.o3 + '<br> pm2_5: '  
+        + polutionInfo.list[0].components.pm2_5 + '<br> pm10: '  
+        + polutionInfo.list[0].components.pm10 + '<br> so2: '  
+        + polutionInfo.list[0].components.so2 + 
+        '</p>' + '<h1>'+ locationInfo.city +'</h1>' 
+
+
+
+
+        console.log(polutionInfo)
+        screenType = 1
+    })
+
+
+    }
+
+    else{
+        checkWeather()
+        document.querySelector('.worldEvil').innerHTML = '<p>World evil</p>'
+        screenType = 0
+    }
+
+}
